e0f8044de5b33e20bb11e224b3824371ad2025b7
APenalveR <angelpenalver@gmail.com>
2025-09-07 12:25:00 -0300

    feat: creación de componentes login y register

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import NavMenu from "./components/Navbar";
+import RegisterPage from "./components/RegisterPage";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <NavMenu />
-      <Home />
+      {/* <Home /> */}
+      <RegisterPage />
+      {/* <Login /> */}
       <Footer />
     </>
   );
